b8c577470512dd7e0cca743841bc8db0f976d6a4
SnowArcher <s.work.viktor@gmail.com>
2023-03-09 13:27:30 +0200

    all services

--- src/components/AllServices.js
+++ src/components/AllServices.js
@@ -1,5 +1,27 @@
 export function AllServices() {
     return (
-        <h1>ALL SERVISES</h1>
+        <div className="all-services">
+            <div className="all-services__item">
+                <img src="../img/interior.svg" alt="interior design" />
+                <h1>Interior Design1</h1>
+                <p>Lorem ipsum dolor amet consectetur
+adipiscing elit sed eiusmod tempor
+incididunt ut labore.</p>
+            </div>
+            <div className="all-services__item">
+                <img src="../img/architecture.svg" alt="interior design" />
+                <h1>Architecture 1</h1>
+                <p>Lorem ipsum dolor amet consectetur
+adipiscing elit sed eiusmod tempor
+incididunt ut labore.</p>
+            </div>
+            <div className="all-services__item">
+                <img src="../img/planning.svg" alt="interior design" />
+                <h1>Planning 1</h1>
+                <p>Lorem ipsum dolor amet consectetur
+adipiscing elit sed eiusmod tempor
+incididunt ut labore.</p>
+            </div>
+        </div>
     )
 }
---
+++ src/components/ServiceItem.js
@@ -0,0 +1,5 @@
+export function ServiceItem() {
+    return (
+        <></>
+    )
+}
